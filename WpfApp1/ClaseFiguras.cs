c4ecb1eea87a9261908f9f10fce49fa496473db3
CESAR LEPE GARCIA <L22212360@tijuana.tecnm.mx>
2023-05-10 10:34:06 -0700

    funciona, pero aun no tiene las animaciones

---
+++ WpfApp1/ClaseFiguras.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp1.Clases
+{
+    abstract class ClaseFiguras
+    {
+
+       public abstract double CalcularArea();
+
+       public abstract double CalcularPerimetro();
+
+        public double Area 
+        {
+            
+            set => _area = value; 
+
+            get => _area; 
+        
+        }
+        public double Perimetro 
+        {
+            
+            set => _perimetro = value; 
+
+            get=> _perimetro; 
+        
+        }
+
+        protected double _area;
+        protected double _perimetro;
+
+    }
+
+    abstract class Paralelogramos : ClaseFiguras
+    {
+
+        public double Altura 
+        {
+            set => _altura = value;
+
+            get => _altura;
+        
+        }
+
+        public double Base
+        {
+            set => _base = value;
+
+            get => _base;
+
+        }
+
+        private double _altura;
+        private double _base;
+
+    }
+
+    class Cuadrado : Paralelogramos
+    {
+        public Cuadrado(double lado)
+        {
+            if (lado > 0)
+            {
+
+                Altura = lado;
+
+            }
+            else
+            {
+
+                Altura = 1;
+
+            }
+
+            CalcularArea();
+            CalcularPerimetro();
+
+        }
+
+        public override double CalcularArea() => CalcularArea(Altura);
+        public double CalcularArea(double lado)
+        {
+            if (lado <= 0)
+            {
+
+                lado = 1;
+
+            }
+
+            Altura = lado;
+
+            Area = Altura * Altura;
+
+            return Area;
+
+        }
+
+        public override double CalcularPerimetro() => CalcularPerimetro(Altura); 
+
+        public double CalcularPerimetro(double lado)
+        {
+
+            if (lado <= 0)
+            {
+
+                lado = 1;
+
+            }
+
+            Altura = lado;
+
+            Perimetro = 4 * Altura;
+
+            return Perimetro;
+
+        }
+
+    }
+
+    class Rectangulo : Paralelogramos
+    {
+        public Rectangulo(double altura, double bbase)
+        {
+            if (altura > 0)
+            {
+                Altura = altura;
+            }
+            else
+            {
+                Altura = 1;
+            }            
+            if (bbase > 0)
+            {
+                Base = bbase;
+            }
+            else
+            {
+                Base = 1;
+            }
+
+            CalcularArea();
+            CalcularPerimetro();
+
+        }
+
+        public override double CalcularArea()
+        {
+
+            Area = CalcularArea(Base, Altura);
+
+            return Area;
+
+        }        
+        public double CalcularArea(double lado1, double lado2)
+        {
+
+            Area = lado1 * lado2;
+
+            return Area;
+
+        }      
+        public override double CalcularPerimetro()
+        {
+
+            Perimetro = CalcularPerimetro(Base, Altura);
+
+            return Perimetro;
+
+        }        
+        public double CalcularPerimetro(double lado1, double lado2)
+        {
+
+            Perimetro = 2*lado1 + 2* lado2;
+
+            return Perimetro;
+
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,21 +27,110 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
-        private void cmiCuadrado_Selected(object sender, RoutedEventArgs e)
+        //private DoubleAnimation Parpadeo()
+        //{
+        //    Storyboard x = new();
+
+        //    DoubleAnimation _animacionParpadeo = new();
+        //    _animacionParpadeo.From = 1;
+        //    _animacionParpadeo.To = 0;
+        //    x.Clone(cb);
+
+        //}
+
+        private Image CambiarImagen(string direccion, Image imagen)
         {
-
-
-
+            BitmapImage myBitmap = new BitmapImage();
+            if (!string.IsNullOrEmpty(direccion))
+            {
+                try
+                {
+                    Uri myUri = new Uri(direccion);
+                    myBitmap.BeginInit();
+                    myBitmap.UriSource = myUri;
+                    myBitmap.EndInit();
+                }
+                catch (System.UriFormatException) { }
+            }
+            imagen.Source = myBitmap;
+            return imagen;
+        }
+        private void HabilitarTextBox(int cantidadTB) 
+        {
+            if (cantidadTB >= 1)
+            {
+                TextBox1.Visibility = Visibility.Visible;
+                TextBox1.IsEnabled = true;
+            }
+            if (cantidadTB >= 2) 
+            {
+                TextBox2.Visibility = Visibility.Visible;
+                TextBox2.IsEnabled = true;
+            }
+            if (cantidadTB >= 3) 
+            {
+                TextBox2.Visibility = Visibility.Visible;
+                TextBox2.IsEnabled = true;
+            }
         }
 
-        private void cbSeleccionarFigura_Initialized(object sender, EventArgs e)
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-            
+            // @"^[0-9](\d*\.?(\d{9})?){1}$"
+            Regex regex = new Regex("[^0-9.]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        private void cbSeleccionarFigura_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbSeleccionarFigura.SelectedItem == cmiCuadrado)
+            {
+                CambiarImagen("C:\\Users\\CESARLEPEGARCIA\\source\\repos\\CesarLepeITT\\Practica1\\WpfApp1\\Imagenes\\Cuadrado.png", MyImagen);
+                HabilitarTextBox(1);
+            }
+            else if (cbSeleccionarFigura.SelectedItem == cmiRectangulo)
+            {
+                CambiarImagen("C:\\Users\\CESARLEPEGARCIA\\source\\repos\\CesarLepeITT\\Practica1\\WpfApp1\\Imagenes\\Rectángulo.png", MyImagen);
+                HabilitarTextBox(2);
+            }
+        }
 
-
+        private void TextBox2_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            double lado1;
+            try
+            {
+                lado1 = double.Parse(TextBox2.Text);
+            }
+            catch (FormatException)
+            {
+                lado1 = 1;
+            }
+        }
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            double lado1;
+            double lado2;
+            if (cbSeleccionarFigura.SelectedItem == cmiCuadrado)
+            {
+                lado1 = double.Parse(TextBox1.Text);
+                Cuadrado myCuadrado = new Cuadrado(lado1);
+                txtArea.Text = myCuadrado.Area.ToString();
+                txtPerimetro.Text = myCuadrado.Perimetro.ToString();
+                CambiarImagen("C:\\Users\\CESARLEPEGARCIA\\source\\repos\\CesarLepeITT\\Practica1\\WpfApp1\\Imagenes\\Rectángulo Area.png", imgArea);
+                CambiarImagen("C:\\Users\\CESARLEPEGARCIA\\source\\repos\\CesarLepeITT\\Practica1\\WpfApp1\\Imagenes\\Cuadrado Perimetro.png", imgPerimetro);
+            }
+            if (cbSeleccionarFigura.SelectedItem == cmiRectangulo)
+            {
+                lado1 = double.Parse(TextBox1.Text);
+                lado2 = double.Parse(TextBox2.Text);
+                Rectangulo myRectangulo = new Rectangulo(lado1 , lado2);
+                txtArea.Text = myRectangulo.Area.ToString();
+                CambiarImagen("C:\\Users\\CESARLEPEGARCIA\\source\\repos\\CesarLepeITT\\Practica1\\WpfApp1\\Imagenes\\Rectángulo Area.png", imgArea);
+                txtPerimetro.Text = myRectangulo.Perimetro.ToString();
+                CambiarImagen("C:\\Users\\CESARLEPEGARCIA\\source\\repos\\CesarLepeITT\\Practica1\\WpfApp1\\Imagenes\\Rectángulo Perimetro.png", imgPerimetro);
+            }
         }
     }
 }
